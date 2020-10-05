a75a6f0937e79640bc68e9edfd22d713f27e33fd
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-05 17:33:37 -0400

    Kyle Thousand

--- repeatString.js
+++ repeatString.js
@@ -1,11 +1,16 @@
 //STRING BEARS REPEATING
 
 const repeatString = (str, num) => {
+  let number = str;
+  while (num > 1) {
+    number += str;
+    num--;
+  }
+  return number;
+};
 
-}
-
-console.log(repeatString("abc", 3))
+console.log(repeatString("abc", 3));
 
 module.exports = {
-  repeatString
-}
+  repeatString,
+};
